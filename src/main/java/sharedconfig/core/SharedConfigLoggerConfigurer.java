9160967269d7b58cf61978aae6d64117ec0bf8e9
Vasypu <V980327u>
2022-06-17 13:21:41 +0300

    Heder and title in log files was changed, format of log files was changed

--- src/main/java/sharedconfig/core/SharedConfigLoggerConfigurer.java
+++ src/main/java/sharedconfig/core/SharedConfigLoggerConfigurer.java
@@ -10,22 +10,48 @@ import org.apache.logging.log4j.core.config.LoggerConfig;
 import org.apache.logging.log4j.core.layout.PatternLayout;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Date;
+import java.math.BigInteger;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class SharedConfigLoggerConfigurer {
     /**
      * Creates logger (named 'sharedconfig') that catches all sharedconfig's logs and targets them to
      * the file located at outputFolderPath
-     * @param appName application name
-     * @param appVersion application version
+     *
+     * @param appName          application name
+     * @param appVersion       application version
      * @param outputFolderPath output logs folder location
      */
-    public static void traceLogsToFile(@NotNull String appName, @NotNull String appVersion, @NotNull String outputFolderPath) {
+    public static void traceLogsToFile(@NotNull String appName, @NotNull String appVersion, @NotNull String outputFolderPath) throws UnknownHostException, ParseException {
         val ctx = (LoggerContext) LogManager.getContext(false);
         val config = ctx.getConfiguration();
-        val layout = PatternLayout.newBuilder().withPattern("TE:%snp{6} t=\"%d{yyyy.MM.dd'T'HH:mm:ss.SSS'Z'}\" k=%-5level m=\"%M : %m\"%n").build();
+//      header
+        var computerName = InetAddress.getLocalHost().getHostName();
+        ProcessHandle handle = ProcessHandle.current();
+        ProcessHandle.Info info = handle.info();
+        var startTime = info.startInstant().isEmpty() ? "" : info.startInstant().get();
+        var pid = handle.pid();
+        var processName = info.command().get().endsWith("java.exe") ? "java.exe" : "";
+        var commandArgs = info.arguments().isEmpty() ? "" : info.arguments().get();
+        var argsHash = getHashMD5(computerName + processName + commandArgs + pid + startTime);
+//      header end
+//      work with date
+        var oldFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        var newFormat = new SimpleDateFormat("yyMMdd-HHmmss");
+        var oldDate = oldFormat.parse(startTime.toString());
+        var newDate = newFormat.format(oldDate);
+//      end work with date
+        val layout = PatternLayout.newBuilder()
+                .withHeader("LI:" + argsHash + " s.MachineName=" + computerName + " s.ProcessName=" + processName
+                        + " s.CommandLine=" + commandArgs + " s.Id=" + pid + " s.StartTime=" + '"' + startTime + '"' + "%n")
+                .withPattern("TE:%snp{6} t=\"%d{yyyy.MM.dd'T'HH:mm:ss.SSS'Z'}\" k=%-5level m=\"%M : %replace{%m}{\\\\}{\\\\\\\\}\"%n").build();
         val appender = FileAppender.newBuilder()
-                .withFileName(outputFolderPath + appName + "_" + appVersion + "_" + new Date().getTime() + ".log")
+                .withFileName(outputFolderPath + newDate + "-" + pid + "-" + argsHash  + "-1-" + "TLOG#" + ".tlog")
                 .setName("sharedconfig-up-logs-appender")
                 .setConfiguration(config)
                 .withBufferedIo(true)
@@ -39,7 +65,7 @@ public class SharedConfigLoggerConfigurer {
         config.addAppender(appender);
 
         val ref = AppenderRef.createAppenderRef("File", null, null);
-        val refs = new AppenderRef[] { ref };
+        val refs = new AppenderRef[]{ref};
 
         val loggerConfig = LoggerConfig.createLogger(true, Level.ALL, "sharedconfig", null, refs, null, config, null);
         loggerConfig.addAppender(appender, Level.TRACE, null);
@@ -53,10 +79,27 @@ public class SharedConfigLoggerConfigurer {
     /**
      * Creates logger (named 'sharedconfig') that catches all sharedconfigs' logs and targets them to
      * the file located at C:\\ProgramData\\universal-platform\\logs\\
-     * @param appName application name
+     *
+     * @param appName    application name
      * @param appVersion application version
      */
-    public static void traceLogsToFileToUPWindowsDefaultLocation(@NotNull String appName, @NotNull String appVersion) {
+    public static void traceLogsToFileToUPWindowsDefaultLocation(@NotNull String appName, @NotNull String appVersion) throws UnknownHostException, ParseException {
         traceLogsToFile(appName, appVersion, "C:\\ProgramData\\universal-platform\\logs\\");
+    }
+
+    /**
+     *
+     * @param string принимает строку для которой нужно расчитать хеш
+     * @return возвращает расчитанный хеш для переданной строки
+     */
+    private static String getHashMD5(String string) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            BigInteger bi = new BigInteger(1, md.digest(string.getBytes()));
+            return bi.toString(16).toUpperCase();
+        } catch (NoSuchAlgorithmException ex) {
+            ex.printStackTrace();
+            return "";
+        }
     }
 }
